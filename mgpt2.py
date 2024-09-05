fcb2c5e6a85e3a790dbc0350095218815878aa35
Mohammed Yaseen <47.yaseen@gmail.com>
2024-09-05 16:09:19 +0530

    dataloader

--- mgpt2.py
+++ mgpt2.py
@@ -1,7 +1,3 @@
-from transformers import PreTrainedTokenizerFast
-
-tokenizer = PreTrainedTokenizerFast.from_pretrained("./tokenizer/mgpt2-tokenizer")
-
 from dataclasses import dataclass
 import torch
 import torch.nn as nn
@@ -10,11 +6,11 @@ import math
 
 @dataclass
 class GPTConfig:
-    block_size: int = 256
-    vocab_size: int = 65
-    n_layer: int = 6
-    n_head: int = 6
-    n_embd: int = 384
+    block_size: int = 1024 # sequence length
+    vocab_size: int = 50257 # number of tokens: 50,000 BPE merges + 256 byte tokens + 1 <|endoftext|> token
+    n_layer: int = 12 # number of layers
+    n_head: int = 12 # number of attention heads
+    n_embd: int = 768 # embedding dimension
 
 class CausalSelfAttention(nn.Module):
     def __init__(self, config) -> None:
@@ -38,7 +34,7 @@ class CausalSelfAttention(nn.Module):
 
         att = q @ k.transpose(-2, -1) * (1.0 / math.sqrt(k.size(-1)))
         att = att.masked_fill(self.bias[:, :, :T, :T] == 0, float("-inf"))
-        att = nn.Softmax(att, dim=-1)
+        att = F.softmax(att, dim=-1)
         y = att @ v
         y = y.transpose(1, 2).contiguous().view(B,T,C)
 
@@ -84,9 +80,29 @@ class GPT(nn.Module):
         ))
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
     
+    def forward(self, idx, targets=None):
+        B, T = idx.size() # (B, T) = batch size, sequence length
+        assert T <= self.config.block_size, f"Cannot forward sequence of length {T}, block size is only {self.config.block_size}"
+
+        pos = torch.arange(0, T, dtype=torch.long, device = idx.device)
+        tok_emb = self.transformer.wte(idx) # (B, T, n_embd)
+        pos_emb = self.transformer.wpe(pos) # (T, n_embd)
+        x = tok_emb + pos_emb # (B, T, n_embd)
+
+        for block in self.transformer.h: 
+            x = block(x)
+
+        x = self.transformer.ln_f(x) # (B, T, n_embd)
+        logits = self.lm_head(x) # (B, T, vocab_size)
+
+        loss = None
+        if targets is not None:
+            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1))
+        
+        return logits, loss
+    
     @classmethod
-    def from_pretrained(cls, model_type
-                        ):
+    def from_pretrained(cls, model_type):
         assert model_type in {'gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl'}
         from transformers import GPT2LMHeadModel
         print(f"loading weights from pretrained gpt {model_type}..")
@@ -116,7 +132,7 @@ class GPT(nn.Module):
         assert len(sd_keys_hf) == len(sd_keys), f"Mismatch: {len(sd_keys_hf)} != {len(sd_keys)}"
         for k in sd_keys_hf:
             if any(k.endswith(suffix) for suffix in transposed_keys):
-                assert sd_hf[k].shape == sd[k].shape
+                assert sd_hf[k].shape[::-1] == sd[k].shape
                 with torch.no_grad():
                     sd[k].copy_(sd_hf[k].T)
             else:
@@ -124,5 +140,83 @@ class GPT(nn.Module):
                 with torch.no_grad():
                     sd[k].copy_(sd_hf[k])
         return model
+# --------------------------------------------------------------------------------
+import tiktoken
+
+class DataLoaderLite:
+    def __init__(self, B, T):
+        self.B = B
+        self.T = T
+        with open('input.txt', 'r') as file:
+            text = file.read()
+        tokens = enc.encode(text)
+        self.tokens = torch.tensor(tokens, dtype=torch.long)
+        print(f"loaded {len(self.tokens)} tokens")
+        print(f"1 epoch: {len(self.tokens) // (B*T)} batches")
+
+        self.current_pos = 0
+    
+    def next_batch(self):
+        B, T = self.B, self.T
+        buf = self.tokens[self.current_pos:self.current_pos + B*T + 1]
+        x = (buf[:-1]).view(B, T)
+        y = (buf[1:]).view(B, T)
+
+        self.current_pos += B*T
+        if self.current_pos + B*T + 1 > len(self.tokens):
+            self.current_pos = 0
         
-        
+        return x, y
+
+    
+# --------------------------------------------------------------------------------
+device = "cpu"
+if torch.cuda.is_available():
+    device = "cuda"
+if hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
+    device = "mps"
+print(f"using device: {device}")
+
+enc = tiktoken.get_encoding('gpt2')
+train_dataloader = DataLoaderLite(B=4, T=32)
+
+# model = GPT.from_pretrained('gpt2')
+model = GPT(GPTConfig())
+model.to(device)
+
+optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
+
+for step in range(50):
+    x, y = train_dataloader.next_batch()
+    x, y = x.to(device), y.to(device)
+    optimizer.zero_grad()
+    logits, loss = model(x, y)
+    loss.backward()
+    optimizer.step()
+    print(f"step {step} loss: {loss.item()}")
+
+import sys; sys.exit(0)
+
+model.eval()
+num_return_sequences = 5 # Batch size: B
+max_length = 30 # Sequence length: T
+tokens = enc.encode("Hello, I'm a language model,")
+tokens = torch.tensor(tokens, dtype=torch.long)
+tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1)
+x = tokens.to(device)
+
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+while x.size(1) < max_length:
+    logits = model(x)  # Shape: (B, T, vocab_size)
+    logits = logits[:, -1, :]  # Shape: (B, vocab_size)
+    probs = F.softmax(logits, dim=-1)  # Shape: (B, vocab_size)
+    topk_probs, topk_indices = torch.topk(probs, 50, dim=-1)  # Both shapes: (B, 50)
+    ix = torch.multinomial(topk_probs, 1)  # Shape: (B, 1)
+    xcol = torch.gather(topk_indices, -1, ix)  # Shape: (B, 1)
+    x = torch.cat([x, xcol], dim=1)  # Shape: (B, T + 1)
+
+for i in range(num_return_sequences):
+    tokens = x[i, :max_length].tolist()
+    decoded = enc.decode(tokens)
+    print(">", decoded)
